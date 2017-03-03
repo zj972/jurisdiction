6a3801f253c532622616020691a83b209b895ec5
zj972 <mail@yangshaobo.cn>
2017-03-03 10:16:24 +0800

    完成title以及侧栏

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="app">
-    <el-col :span="24">
-      <el-menu router>
-        <el-menu-item
-          v-for="(list, index) in lists"
-          :index="list.address"
-          v-text="list.name">
-        </el-menu-item>
-      </el-menu>
-    </el-col>
-    <router-view></router-view>
+    <el-row class="title">
+      <el-col :xs="24" :sm="4" :md="3" :lg="2">权限中心</el-col>
+    </el-row>
+    <el-row>
+      <el-col :xs="24" :sm="4" :md="3" :lg="2">
+        <el-menu router class="list"><!-- :default-active="lists[0].address" 默认选中第一个，需要修改index.js中lists的path:'/' -->
+          <el-menu-item
+            v-for="(list, index) in lists"
+            :index="list.address"
+            v-text="list.name">
+          </el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-col :xs="24" :sm="20" :md="21" :lg="22">
+        <router-view></router-view>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -42,7 +49,19 @@ export default {
 </script>
 
 <style>
+.title{
+  background: #8492A6;
+  color: #fff;
+  line-height: 56px;
+  text-align: center;
+}
+
 .el-menu-item{
   text-align: center;
+}
+
+.list>.is-active{
+  background: #d1dbe5;
+  color: #fff;
 }
 </style>
